84ba876ec858c0cda274a4d13164456693a8abf1
Alex <a.serb2014@yandex.ru>
2024-07-16 22:10:08 +0300

    add_get_data_

--- classes/vk.py
+++ classes/vk.py
@@ -2,12 +2,12 @@ import yaml
 import vk_api
 from vk_api.longpoll import VkLongPoll, VkEventType
 
-with open('..\settings.yaml', 'r') as f:
+with open('..\\settings.yaml', 'r') as f:
     yaml_read = yaml.load(f, Loader=yaml.SafeLoader)
-    token = yaml_read['vk']['token']
-f.close()
+    token_vk = yaml_read['vk']['token']
+    f.close()
 
-vk_session = vk_api.VkApi(token=token)
+vk_session = vk_api.VkApi(token=token_vk)
 vk = vk_session.get_api()
 longpoll = VkLongPoll(vk_session)
 
@@ -24,6 +24,16 @@ class VK:
         city = user_info_get[0]['city']['title'] if 'city' in user_info_get[0] else None #Возвращет None, если не указан город
         return {'id': self.id_vk, 'date': date, 'sex': sex, 'city': city}
 
+
+if __name__ == '__main__':
+
+    for event in longpoll.listen():
+        if event.type == VkEventType.MESSAGE_NEW:
+            if event.to_me:
+                msg = event.text.lower()
+                id_vk = event.user_id
+                vk.messages.send(user_id=id_vk, message=msg, random_id=0)
+                vkid = VK(id_vk)
 
 
 
