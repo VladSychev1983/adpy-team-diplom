d750763612dfdbab8c7d23628ee2e6be3b257892
Alex <a.serb2014@yandex.ru>
2024-07-16 21:39:24 +0300

    gitignore

--- classes/vk.py
+++ classes/vk.py
@@ -1,1 +1,15 @@
-import 
+import yaml
+import vk_api
+from vk_api.longpoll import VkLongPoll, VkEventType
+
+with open('..\settings.yaml', 'r') as f:
+    yaml_read = yaml.load(f, Loader=yaml.SafeLoader)
+    token = yaml_read['vk']['token']
+f.close()
+
+
+
+
+
+
+
