b4d5159f5577c89929aedfb15f94e3ae69e516fb
Vladislav Sychev <vladsychev1983@gmail.com>
2024-07-29 11:01:18 +0300

    Added postgres sql schema, new pgdump.sql

--- pgdump.sql
+++ pgdump.sql
@@ -2,8 +2,8 @@
 -- PostgreSQL database dump
 --
 
--- Dumped from database version 16.2
--- Dumped by pg_dump version 16.2
+-- Dumped from database version 16.3 (Ubuntu 16.3-0ubuntu0.24.04.1)
+-- Dumped by pg_dump version 16.3 (Ubuntu 16.3-0ubuntu0.24.04.1)
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -19,18 +19,6 @@ SET row_security = off;
 SET default_tablespace = '';
 
 SET default_table_access_method = heap;
-
---
--- Name: VK_Favorit; Type: TABLE; Schema: public; Owner: postgres
---
-
-CREATE TABLE public."VK_Favorit" (
-    id_user_vk integer NOT NULL,
-    id_favorit_vk integer NOT NULL
-);
-
-
-ALTER TABLE public."VK_Favorit" OWNER TO postgres;
 
 --
 -- Name: favorits; Type: TABLE; Schema: public; Owner: postgres
@@ -64,6 +52,41 @@ ALTER SEQUENCE public.favorits_id_favorit_seq OWNER TO postgres;
 --
 
 ALTER SEQUENCE public.favorits_id_favorit_seq OWNED BY public.favorits.id_favorit;
+
+
+--
+-- Name: vk_favorit; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.vk_favorit (
+    id integer NOT NULL,
+    id_user_vk integer NOT NULL,
+    id_favorit_vk integer NOT NULL
+);
+
+
+ALTER TABLE public.vk_favorit OWNER TO postgres;
+
+--
+-- Name: vk_favorit_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.vk_favorit_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER SEQUENCE public.vk_favorit_id_seq OWNER TO postgres;
+
+--
+-- Name: vk_favorit_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.vk_favorit_id_seq OWNED BY public.vk_favorit.id;
 
 
 --
@@ -108,6 +131,13 @@ ALTER TABLE ONLY public.favorits ALTER COLUMN id_favorit SET DEFAULT nextval('pu
 
 
 --
+-- Name: vk_favorit id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.vk_favorit ALTER COLUMN id SET DEFAULT nextval('public.vk_favorit_id_seq'::regclass);
+
+
+--
 -- Name: vk_id id_user; Type: DEFAULT; Schema: public; Owner: postgres
 --
 
@@ -115,18 +145,23 @@ ALTER TABLE ONLY public.vk_id ALTER COLUMN id_user SET DEFAULT nextval('public.v
 
 
 --
--- Data for Name: VK_Favorit; Type: TABLE DATA; Schema: public; Owner: postgres
---
-
-COPY public."VK_Favorit" (id_user_vk, id_favorit_vk) FROM stdin;
-\.
-
-
---
 -- Data for Name: favorits; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
 COPY public.favorits (id_favorit, id_favorit_vk) FROM stdin;
+1	132230362
+2	120646597
+3	96125796
+\.
+
+
+--
+-- Data for Name: vk_favorit; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.vk_favorit (id, id_user_vk, id_favorit_vk) FROM stdin;
+1	1	2
+2	1	3
 \.
 
 
@@ -135,6 +170,7 @@ COPY public.favorits (id_favorit, id_favorit_vk) FROM stdin;
 --
 
 COPY public.vk_id (id_user, id_user_vk) FROM stdin;
+1	2379835
 \.
 
 
@@ -142,22 +178,21 @@ COPY public.vk_id (id_user, id_user_vk) FROM stdin;
 -- Name: favorits_id_favorit_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
 
-SELECT pg_catalog.setval('public.favorits_id_favorit_seq', 1, false);
+SELECT pg_catalog.setval('public.favorits_id_favorit_seq', 3, true);
+
+
+--
+-- Name: vk_favorit_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.vk_favorit_id_seq', 2, true);
 
 
 --
 -- Name: vk_id_id_user_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
 
-SELECT pg_catalog.setval('public.vk_id_id_user_seq', 1, false);
-
-
---
--- Name: VK_Favorit VK_Favorit_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY public."VK_Favorit"
-    ADD CONSTRAINT "VK_Favorit_pkey" PRIMARY KEY (id_user_vk, id_favorit_vk);
+SELECT pg_catalog.setval('public.vk_id_id_user_seq', 1, true);
 
 
 --
@@ -169,6 +204,14 @@ ALTER TABLE ONLY public.favorits
 
 
 --
+-- Name: vk_favorit vk_favorit_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.vk_favorit
+    ADD CONSTRAINT vk_favorit_pkey PRIMARY KEY (id);
+
+
+--
 -- Name: vk_id vk_id_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -177,19 +220,19 @@ ALTER TABLE ONLY public.vk_id
 
 
 --
--- Name: VK_Favorit VK_Favorit_id_favorit_vk_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+-- Name: vk_favorit vk_favorit_id_favorit_vk_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
-ALTER TABLE ONLY public."VK_Favorit"
-    ADD CONSTRAINT "VK_Favorit_id_favorit_vk_fkey" FOREIGN KEY (id_favorit_vk) REFERENCES public.favorits(id_favorit);
+ALTER TABLE ONLY public.vk_favorit
+    ADD CONSTRAINT vk_favorit_id_favorit_vk_fkey FOREIGN KEY (id_favorit_vk) REFERENCES public.favorits(id_favorit);
 
 
 --
--- Name: VK_Favorit VK_Favorit_id_user_vk_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+-- Name: vk_favorit vk_favorit_id_user_vk_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
-ALTER TABLE ONLY public."VK_Favorit"
-    ADD CONSTRAINT "VK_Favorit_id_user_vk_fkey" FOREIGN KEY (id_user_vk) REFERENCES public.vk_id(id_user);
+ALTER TABLE ONLY public.vk_favorit
+    ADD CONSTRAINT vk_favorit_id_user_vk_fkey FOREIGN KEY (id_user_vk) REFERENCES public.vk_id(id_user);
 
 
 --
